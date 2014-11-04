969d3abaad7152c07797a536b4c985c14b60bd82
timakin <timaki.st@gmail.com>
2014-11-05 02:15:15 +0900

    path

--- lib/crunchbase_data_analysis.rb
+++ lib/crunchbase_data_analysis.rb
@@ -1,5 +1,6 @@
-require 'crunchbase_data_analysis/common'
-require 'crunchbase_data_analysis/get_cross_section_csvdata'
+require File.expand_path('./crunchbase_data_analysis/common', __FILE__)
+require File.expand_path('./crunchbase_data_analysis/get_cross_section_csvdata', __FILE__)
+
 
 module CrunchbaseDataAnalysis
 end
