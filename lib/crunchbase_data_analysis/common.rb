c008c2d64788a2682d2089113b52331d32c4d777
timakin <timaki.st@gmail.com>
2014-11-06 00:32:42 +0900

    decline session number

--- lib/crunchbase_data_analysis/common.rb
+++ lib/crunchbase_data_analysis/common.rb
@@ -23,9 +23,8 @@ module CrunchbaseDataAnalysis
     total = (list ? list['paging']['total_items'] : 0)
   end
 
-  def get_age(permalink)
+  def get_age(person)
     p "get_age"
-    person = get_person(permalink)
     if person.born_on_year.nil?
       age = 25
     elsif
@@ -44,55 +43,52 @@ module CrunchbaseDataAnalysis
     (value ? value : 0)
   end
 
-  def founded_org_path_list(permalink, org_list=[])
+  def founded_org_path_list(person, raw_org_list=[], result_org_list=[])
     p "founded_org_path_list"
-    person = get_person(permalink)
     items = person.founded_company['items']
     items.each do |founded|
-      org_list << (founded['path']).slice!(13..-1)
+      raw_org_list << (founded['path']).slice!(13..-1)
     end
-    org_list.compact
+    raw_org_list.compact.each do |org|
+      result_org_list << get_org(org)
+    end
+    result_org_list
   end
 
-  def count_of_exp(permalink)
+  def count_of_exp(person)
     p "count_of_exp"
-    person = get_person(permalink)
     experience_of_person = person.experience
     total_count_of_exp_org = get_total_num(experience_of_person)
   end
 
-  def not_founder_flag(permalink)
+  def not_founder_flag(person)
     p "not_founder_flag"
-    person = get_person(permalink)
     condition = person.founded_company.nil?
     bool_to_int(condition) 
   end
 
-  def serial_flag(permalink)
+  def serial_flag(person)
     p "serial_flag"
-    person = get_person(permalink)
     num_of_founded = get_total_num(person.founded_company)
     condition = !!(num_of_founded >= 2)
     bool_to_int(condition)
   end
 
-  def vp_flag(permalink)
+  def vp_flag(person)
     p "vp_flag"
-    person = get_person(permalink)
     condition = !person.advisor_at.nil?
     bool_to_int(condition)
   end
 
-  def inv_flag(permalink)
+  def inv_flag(person)
     p "inv_flag"
-    person = get_person(permalink)
     condition = !person.investments.nil?
     bool_to_int(condition)
   end
 
-  def biz_flag(permalink)
+  def biz_flag(person)
     p "biz_flag"
-    list = degree_sub_list(permalink)
+    list = degree_sub_list(person)
     for biz in @@biz_list
       condition = bool_to_int(list.include?(biz))
       break if condition
@@ -100,9 +96,9 @@ module CrunchbaseDataAnalysis
     condition
   end
 
-  def cs_flag(permalink)
+  def cs_flag(person)
     p "cs_flag"
-    list = degree_sub_list(permalink)
+    list = degree_sub_list(person)
     for cs in @@cs_list
       condition = bool_to_int(list.include?(cs))
       break if condition
@@ -110,9 +106,9 @@ module CrunchbaseDataAnalysis
     condition
   end
 
-  def master_flag(permalink)
+  def master_flag(person)
     p "master_flag"
-    list = degree_type_list(permalink)
+    list = degree_type_list(person)
     for master in @@master_list
       condition = bool_to_int(list.include?(master))
       break if condition
@@ -120,8 +116,7 @@ module CrunchbaseDataAnalysis
     condition
   end
 
-  def degree_sub_list(permalink, list=[])
-    person = get_person(permalink)
+  def degree_sub_list(person, list=[])
     degree_param = (!person.degrees.nil? ? person.degrees['items'] : [])
     degree_param.each do |deg|
       subject ||= deg['degree_subject']
@@ -130,8 +125,7 @@ module CrunchbaseDataAnalysis
     list.compact
   end
 
-  def degree_type_list(permalink, list=[])
-    person = get_person(permalink)
+  def degree_type_list(person, list=[])
     degree_param = (!person.degrees.nil? ? person.degrees['items'] : [])
     degree_param.each do |deg|
       type_name ||= deg['degree_type_name']
@@ -143,7 +137,7 @@ module CrunchbaseDataAnalysis
   def total_funding(org_list, total = 0)
     p "total_funding"
     org_list.each do |org|
-      total += nil_addition_guard(get_org(org).total_funding_usd)
+      total += nil_addition_guard(org.total_funding_usd)
     end
     total
   end
@@ -151,7 +145,7 @@ module CrunchbaseDataAnalysis
   def count_of_product(org_list, total = 0)
     p "count_of_product"
     org_list.each do |org|
-      total += nil_addition_guard(get_total_num(get_org(org).products))
+      total += nil_addition_guard(get_total_num(org.products))
     end
     total
   end
@@ -159,7 +153,7 @@ module CrunchbaseDataAnalysis
   def count_of_news(org_list, total = 0)
     p "count_of_news"
     org_list.each do |org|
-      total += nil_addition_guard(get_total_num(get_org(org).news))
+      total += nil_addition_guard(get_total_num(org.news))
     end
     total
   end
--- lib/crunchbase_data_analysis/get_cross_section_csvdata.rb
+++ lib/crunchbase_data_analysis/get_cross_section_csvdata.rb
@@ -7,13 +7,13 @@ module CrunchbaseDataAnalysis
       permalink_path = File.expand_path('crunchbase_data_analysis/crunchbase_data/permalink.csv')
       CSV.open(cross_path, 'w') do |row|
         row << ['permalink', 'fr_sum', 'serial_dummy', 'vp_exp_dummy', 'num_comp_exp', 'product_num', 'num_news', 'inv_dummy', 'cs_dummy', 'biz_dummy', 'master_dummy', 'age']
-        CSV.foreach(permalink_path) do |l|
+        CSV.foreach(permalink_path) do |rawlink|
           p "======================"
-          link = l[0]
+          link = get_person(rawlink[0])
           next if not_founder_flag(link) == 1
           puts "Founder flag passed"
           org_list = founded_org_path_list(link)
-          row << [link, total_funding(org_list), serial_flag(link), vp_flag(link), count_of_exp(link), count_of_product(org_list), count_of_news(org_list), inv_flag(link), cs_flag(link), biz_flag(link), master_flag(link), get_age(link)]
+          row << [link.permalink, total_funding(org_list), serial_flag(link), vp_flag(link), count_of_exp(link), count_of_product(org_list), count_of_news(org_list), inv_flag(link), cs_flag(link), biz_flag(link), master_flag(link), get_age(link)]
         end
       end
     end
